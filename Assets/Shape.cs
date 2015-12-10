c51e0e2d71c819af6485c8c64a4100706b317ddf
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2015-12-10 13:48:30 -0800

    commit

---
+++ Assets/Shape.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Shape : MonoBehaviour {
+
+	[SerializeField, HideInInspector] 
+	ParticleSystem particle;
+
+	IEnumerator Start () 
+	{
+		particle = GetComponent<ParticleSystem>();
+
+		while(true)
+		{
+			yield return new WaitWhile(()=> Input.anyKeyDown == false) ;
+			yield return null;
+			particle.startSize = 0.5f;
+
+			yield return new WaitWhile(()=> Input.anyKeyDown == false) ;
+			yield return null;
+			particle.startSize = 2;
+		}
+	}
+}
